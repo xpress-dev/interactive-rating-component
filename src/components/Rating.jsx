cc4644b2b5970dc92104d3d652f34a0c0c427838
Ali Akbarzadeh <akbarzadeh.ali77@gmail.com>
2023-04-06 20:56:08 +0430

    implemented rating functionality

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
 import starIcon from '../assets/icon-star.svg';
 import RatingCount from './RatingCount';
+import ThankYou from './ThankYou';
 const Rating = () => {
+  const [selectedRatingCount, setSelectedRatingCount] = useState(null);
+  const [showThankYou, setShowThankYou] = useState(false);
+  const ratings = [1, 2, 3, 4, 5];
   return (
-    <article className='bg-[#252d37] w-[400px] rounded-3xl h-[395px] p-8 flex flex-col gap-5'>
-      <div className=' bg-[#7c879835] w-12 h-12 flex justify-center items-center  rounded-full'>
-        <img src={starIcon} className='rounded-full ' />
-      </div>
-      <h1 className='text-white font-bold text-[1.75rem]'>How did we do?</h1>
-      <p className='text-[#959eac]'>
-        Please let us know how we did with your support request. All feedback is
-        appreciated to help us improve our offering!
-      </p>
-      <div className='flex w-full justify-between'>
-        <RatingCount amount='1' />
-        <RatingCount amount='2' />
-        <RatingCount amount='3' />
-        <RatingCount amount='4' />
-        <RatingCount amount='5' />
-      </div>
-      <button className=' font-serif text-white bg-[#fb7413] py-2 rounded-full'>
-        S U B M I T
-      </button>
-    </article>
+    <div>
+      {showThankYou && selectedRatingCount ? (
+        <ThankYou />
+      ) : (
+        <article className='bg-[#252d37] w-[400px] rounded-3xl h-[395px] p-8 flex flex-col gap-5'>
+          <div className=' bg-[#7c879835] w-12 h-12 flex justify-center items-center  rounded-full'>
+            <img src={starIcon} className='rounded-full ' />
+          </div>
+          <h1 className='text-white font-bold text-[1.75rem]'>
+            How did we do?
+          </h1>
+          <p className='text-[#959eac]'>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+          <div className='flex w-full justify-between'>
+            {ratings.map((rating) => (
+              <RatingCount
+                amount={rating}
+                currentCount={selectedRatingCount}
+                ratingCountHandler={setSelectedRatingCount}
+              />
+            ))}
+          </div>
+          <button
+            onClick={() => {
+              if (selectedRatingCount) {
+                setShowThankYou(true);
+              }
+            }}
+            className=' font-serif text-white bg-[#fb7413] active:text-[#fb7413] active:bg-white py-2 rounded-full'
+          >
+            S U B M I T
+          </button>
+        </article>
+      )}
+    </div>
   );
 };
 
--- src/components/RatingCount.jsx
+++ src/components/RatingCount.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const RatingCount = ({ amount }) => {
+const RatingCount = ({ amount, ratingCountHandler, currentCount }) => {
   return (
-    <button className='text-white/80 bg-[#7c879835] w-12 h-12 rounded-full flex justify-center items-center hover:bg-[#7c8798]'>
+    <button
+      onClick={() => {
+        ratingCountHandler(amount);
+      }}
+      className={`text-white/80 w-12 h-12 rounded-full flex justify-center items-center hover:bg-[#7c8798] active:bg-[#fb7413] ${
+        +amount === +currentCount
+          ? 'bg-[#fb7413] hover:bg-[#fb7413]'
+          : 'bg-[#7c879835]'
+      }`}
+    >
       {amount}
     </button>
   );
